8d431a5898900a978b84cc71582837ecbdf921b2
kodashim <kodashim@yahoo-corp.jp>
2020-01-04 02:17:41 +0900

    aboutの追加

---
+++ src/components/AboutSkill.vue
@@ -0,0 +1,216 @@
+<template>
+  <section class="skill">
+    <header>
+      <h2>Skill</h2>
+      <div class="tip">
+        <i>i</i>
+        <ul>
+          <li class="rate-3">
+            <p>人に教えられる</p>
+          </li>
+          <li class="rate-2">
+            <p>1人でできる</p>
+          </li>
+          <li class="rate-1">
+            <p>経験したことがある</p>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <section v-for="(obj, index) in skill" :key="index">
+      <h3>{{obj.title}}</h3>
+      <ul>
+        <li v-for="(item, index2) in obj.list" :key="index2" :class="'rate-'+item.rate">
+          {{item.text}}
+        </li>
+      </ul>
+    </section>
+  </section>
+</template>
+
+<script>
+import skill from '../data/skill.json'
+
+export default {
+  name: 'AboutSkill',
+  data: () => {
+    return{
+      skill: skill
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Oswald:300,600&display=swap');
+
+.skill{
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  max-width: 950px;
+  margin: 100px auto 40px;
+  align-items: baseline;
+
+  header{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    .tip{
+      position: absolute;
+      right: 0;
+      i{
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 12px;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        box-shadow: 0 0 6px 0 rgba(0,0,0,.3);
+      }
+      ul {
+        display: none;
+        position: absolute;
+        padding-top: 10px;
+        line-height: 1.6em;
+        color: #fff;
+        width: 190px;
+        li {
+          text-align: right;
+          padding-left: 8px;
+          width: 100px;
+          z-index: 1;
+          font-size: 12px;
+          margin-bottom: 4px;
+          p{
+            margin-right: -12px;
+          }
+        }
+        .rate-1::after{
+          right: -38px;
+        }
+        .rate-2::after{
+          right: -55px;
+        }
+        .rate-3::after{
+          right: -72px;
+        }
+        &::after{
+          content: "";
+          display: block;
+          border-radius: 5px;
+          opacity: .1;
+          background: #000;
+          width: 190px;
+          height: 96px;
+          position: absolute;
+          top: 1px;
+        }
+        &:before {
+          content: "";
+          position: absolute;
+          top: 100%;
+          left: 50%;
+          border: 15px solid transparent;
+          opacity: .1;
+          border-top: 15px solid #000;
+          margin-left: -15px;
+        }
+      }
+      &:hover ul{
+        cursor: pointer;
+        display: flex;
+        top: -116px;
+        left: -85px;
+      }
+    }
+  }
+
+  h2 {
+    display: flex;
+    justify-content: center;
+    flex-direction: column-reverse;
+    align-items: center;
+    font-family: 'Oswald';
+    font-size: 20px;
+    color: #999;
+    margin-left: 20px;
+
+    &::before{
+      content: '';
+      margin-top: 8px;
+      display: block;
+      width: 50px;
+      height: 2px;
+      background: #C4C4C4;
+    }
+  }
+
+  section{
+    margin: 40px 0;
+    text-align: right;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    padding-right: 80px;
+
+    h3{
+      font-size: 14px;
+      color: #666;
+      margin-bottom: 20px;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      &::after{
+        content: '';
+        margin-top: 4px;
+        display: block;
+        width: 50px;
+        height: 1px;
+        background: #ccc;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+
+    li {
+      margin-bottom: 12px;
+      font-size: 14px;
+      color: #666;
+    }
+
+    .rate-{
+      &1, &2, &3{
+        position: relative;
+        display: inline-block;
+        &::after, &::before{
+          content: '★★★';
+          position: absolute;
+          right: -72px;
+          top:0;
+          color: #FFA500;
+          letter-spacing: 0.4em;
+        }
+        &::before{
+          color: #ccc;
+        }
+      }
+      &1::after{
+        content: '★';
+        right: -33px;
+      }
+      &2::after{
+        content: '★★';
+        right: -52px;
+      }
+    }
+  }
+}
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,41 +1,45 @@
 <template>
-  <body class="about">
-    <section class="profile">
+  <main>
+    <div class="profile">
       <img :src="prof" alt="">
-      <h2>
-        Odashima Kei
-        <span>小田島 慧</span>
-      </h2>
-      <p>大学で情報デザインを専攻、卒業後はヤフー株式会社のデザイナーとして2017年に入社。新規事業のサービスデザインに携わる。現在はスポーツナビのアプリやウェブのデザインとコーディングを担当。副業では新規サービスのUIデサインとコーディングを担当。</p>
+      <h1>
+        <p>小田島<span>慧</span></p>
+        <p>Odashima<span>Kei</span></p>
+      </h1>
+      <p>3年目の社会人デザイナー。ストレングスファインダーの最上位資質は学習欲。業務はデザインから開発まで担当。最近はデザインシステムを作ることが多い。目標はデザインを軸に経営から開発までを推進できるようになること。写真の飲み物は麦茶。あだ名はおだし。</p>
+    </div>
+
+    <section class="career">
+      <h2>Career</h2>
+      <ol>
+        <li>
+          <p>ヤフーに新卒デザイナーとして入社。新規事業のサービスのデザインと開発を担当。</p>
+        </li>
+        <li>
+          <p>10月にスポーツナビへ出向し、Webとアプリのデザインと開発を担当。<br>副業で、複数の新規事業の立ち上げのデザインと開発を担当。現在も継続中。</p>
+        </li>
+        <li>
+          <p>サービスのリデザインPJでデザインと開発を担当。<br>副業で、デザイン未経験者をWebデザイナーとして鍛え、デザイン会社の内定を頂く。</p>
+        </li>
+        <li>
+          <p>ヤフー全社の東京オリンピック特集のデザインと開発を担当中。</p>
+        </li>
+      </ol>
     </section>
 
-    <section class="skill">
-      <section>
-        <h3>Design</h3>
-        <ul>
-          <li>Information Design</li>
-          <li>Visual Design</li>
-          <li>Motion Design</li>
-        </ul>
-      </section>
+    <AboutSkill />
 
-      <section>
-        <h3>Cording</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScrip</li>
-        </ul>
-      </section>
-    </section>
-
-  </body>
+  </main>
 </template>
 
 <script>
+import AboutSkill from '@/components/AboutSkill.vue'
 
 export default {
   name: 'about',
+  components: {
+    AboutSkill
+  },
   data: () => {
     return{
       prof: "./assets/prof.jpg",
@@ -45,47 +49,140 @@ export default {
 </script>
 
 <style scoped lang="scss">
-img{
-  display: block;
-  margin: 0 auto;
-  width: 150px;
-  border-radius: 50%;
-  margin-top: 80px;
-}
-h2{
-  font-size: 36px;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  margin-top: 16px;
-}
-span{
-  font-size: 20px;
-  margin-top: 16px;
-}
-p{
-  max-width: 700px;
-  margin: 0 20px;
-  margin-top: 46px;
-  line-height: 1.5;
-}
+@import url('https://fonts.googleapis.com/css?family=Oswald:300,600&display=swap');
+
 .profile{
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  flex-flow: column;
-}
-.skill{
-  display: flex;
-  justify-content: center;
-  margin: 60px 20px;
-  section{
-    margin: 0 100px;
+  max-width: 900px;
+  margin: 100px auto 40px;
+  position: relative;
+
+  img{
+    display: block;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    position: relative;
+    z-index: 1;
+  }
+
+  &::after{
+    z-index: 0;
+    position: absolute;
+    content: "";
+    left: -12px;
+    top: -12px;
+    display: block;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background: #EEE;
+  }
+
+  h1{
+    display: flex;
+    flex-direction:column-reverse;
+    justify-content: space-between;
+    font-weight: bold;
+    height: 80px;
+    p{
+      display: flex;
+      justify-content: space-between;
+      text-align: justify;
+      &:first-child{
+        color: #666;
+        font-size: 24px;
+        letter-spacing: 0.3em;
+        font-family: 'Hiragino Kaku Gothic ProN';
+      }
+      &:last-child{
+        color: #999;
+        font-family: 'Oswald', sans-serif;
+        font-size: 12px;
+        letter-spacing: 0.5em;
+        text-transform: uppercase;
+      }
+      span{
+        font-weight: 100;
+        margin-left: 8px;
+      }
+    }
+    &:before{
+      margin-top: 8px;
+      display: block;
+      content: "";
+      width: 50px;
+      height: 4px;
+      background: #999;
+    }
+  }
+
+  p{
+    max-width: 590px;
+    font-size: 14px;
+    line-height: 26px;
+    text-align: justify;
   }
 }
-ul{
-  margin: 24px 0;
+
+.career{
+  display: flex;
+  flex-direction: column;
+  max-width: 950px;
+  margin: 100px auto 40px;
+
+  h2 {
+    display: flex;
+    justify-content: center;
+    flex-direction: column-reverse;
+    align-items: center;
+    font-family: 'Oswald';
+    font-size: 20px;
+    color: #999;
+    &::before{
+      content: '';
+      margin-top: 8px;
+      display: block;
+      width: 50px;
+      height: 2px;
+      background: #C4C4C4;
+    }
+  }
+
+  ol {
+    max-width: 950px;
+    margin: 40px auto;
+    counter-reset: count 2016;
+    li {
+      display: flex;
+      margin-bottom: 20px;
+      &::before{
+        counter-increment: count;
+        content:counter(count);
+        margin-top: 6px;
+        font-weight: bold;
+        font-size: 12px;
+        color: #999;
+      }
+      p {
+        margin-left: 20px;
+        display: flex;
+        font-size: 14px;
+        line-height: 26px;
+        &::before{
+          content: '';
+          margin-right: 20px;
+          display: block;
+          margin-top: 10px;
+          width: 12px;
+          height: 2px;
+          background: #C4C4C4;
+        }
+      }
+    }
+  }
 }
-li{
-  margin: 12px 0;
-}
+
 </style>
